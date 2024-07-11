4379156e3f70efd05977151d592c02a9b0a7506a
dogukan <dogukan.karaduman@eltturkey.com>
2024-07-12 01:51:18 +0300

    Updated app some part

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="real-estate-app">
+    <router-view />
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "real-estate-app",
+  async created() {
+
+    // setting initial state via airtable dataset
+    await this.setContactsList();
+    await this.setAgentsList();
+    await this.setAppointmentsList();
+
+  },
+  computed: {
+    ...mapGetters(["getContactsList", "getAgentsList", "getAppointmentsList"]),
+  },
+  methods: {
+   async setContactsList() {
+
+      if(this.getContactsList.length === 0){
+        await this.$store.dispatch("getContactsList");
+      }
+
+    },
+    async setAgentsList() {
+      if(this.getAgentsList.length === 0){
+        await this.$store.dispatch("getAgentsList");
+      } 
+    },
+    async setAppointmentsList() {
+      if(this.getAppointmentsList.length === 0){
+        await this.$store.dispatch("getAppointmentsList");
+      } 
+    },
+  },
 }
+</script>
+<style lang="scss" scoped>
+// @import "@/assets/scss/app.scss";
+// @import "@/assets/scss/scroll.scss";
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+* {
+  font-family: "Open Sans";
+  font-style: normal;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
+const dotenv = require('dotenv')
+dotenv.config()
 module.exports = defineConfig({
-  transpileDependencies: true,
-});
+
+  lintOnSave: false,
+  pluginOptions:{
+    env: {
+    
+    }
+  },
+
+  devServer: {
+    client: {
+      logging: 'none',
+      overlay: false,
+    },
+  
+  },
+  transpileDependencies: ["vue-notion"],
+
+
+
+})
