3a608ee0a8483857bb9e718e65512586956e06b8
dogukan <dogukan.karaduman@eltturkey.com>
2024-07-15 04:39:22 +0300

    main.js file has been finalized. Added dependencies.

--- src/main.js
+++ src/main.js
@@ -2,5 +2,28 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import LoadingSplash from "./utils/LoadingSplash.vue"
 
-createApp(App).use(store).use(router).mount("#real-estate-app");
+import {
+    // create naive ui
+    create,
+    // component
+    NSelect,
+    NTreeSelect,
+    NPopselect,
+    NPagination,
+    NSpace
+   
+  } from 'naive-ui'
+  
+  const naive = create({
+    components: [NPagination, NPopselect,NSelect,NTreeSelect,NSpace]
+  })
+
+import DatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+createApp(App).use(store).use(naive).use(router).
+component("date-picker", DatePicker).
+component("LoadingSplash", LoadingSplash).
+mount("#real-estate-app");
