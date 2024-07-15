85d3c54cd33f5bcf4a70f3c5dab4abc485830d7e
dogukan <dogukan.karaduman@eltturkey.com>
2024-07-15 04:36:47 +0300

    Home page finalized with all related parts.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home-page-container">
+    <NavbarApp />
+    <AppointmetsListContainer :key="getModalStatus" />
+    <Teleport to="body" v-if="getModalStatus">
+      <AppointmentActionModal />
+    </Teleport>
+    
+
   </div>
 </template>
 
 <script>
+
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import NavbarApp from '@/components/navbar_app/IndexNavbarApp.vue';
+import AppointmetsListContainer from '@/components/appointments_list/IndexAppointmentsList.vue'
+import AppointmentActionModal from "@/components/appointment_modal_container/IndexAppointmentModal.vue";
+import { mapGetters } from "vuex";
+import {computed} from 'vue'
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    NavbarApp,
+    AppointmetsListContainer,
+    AppointmentActionModal
+  },
+  provide() {
+    return {
+      appointmentActionType: computed(() => this.getAppointmentModalType),
+    };
+  },
+
+  computed: {
+    ...mapGetters(["getAppointmentsListFilter", "getModalStatus","getAppointmentModalType"]),
   },
 };
 </script>
+
+<style scoped lang="scss">
+.home-page-container {
+  display: flex;
+  flex-direction: column;
+  height: 99vh;
+  width: 96%;
+}
+</style>
